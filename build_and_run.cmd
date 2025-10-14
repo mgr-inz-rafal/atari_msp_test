841d80ed372d0b487492ba1fbff76ce5703cf158
mgr-inz-rafal <rchabowski@gmail.com>
2025-10-14 22:30:17 +0200

    First drop

---
+++ build_and_run.cmd
@@ -0,0 +1,7 @@
+REM Pascal
+mp.exe src\main.pas -code:2000 -ipath:madpascal\lib -o:bin\main.a65
+
+REM Assemble
+mads.exe bin\main.a65 -x -i:madpascal\base -o:bin\main.xex -l:bin\main.lst -t:bin\main.lab 
+
+call bin\main.xex
